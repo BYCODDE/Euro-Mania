f17641b30b047865af041ea0e6abbce973293684
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-08-27 16:48:51 +0400

    style: adding cursor to header!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,11 @@ import humburger from "/assets/svg/humburger.svg";
 import cart from "/assets/svg/cart.svg";
 function Header() {
   return (
-    <header className="font-playfair p-[24px] pt-[37px] pb-[37px] w-full h-full bg-black text-white flex justify-between">
-      <img src={humburger} alt="humburger" />
+    <header className="font-playfair p-[24px] pt-[37px] pb-[37px] w-full h-full bg-black text-white flex justify-between text-[30px]">
+      <img src={humburger} alt="humburger" className="cursor-pointer" />
       Euro Mania
-      <img src={cart} alt="cart" />
+      <img src={cart} alt="cart" className="cursor-pointer" />
+      
     </header>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ function Home() {
     <div>
       <Header />
       Home
+      {/* <img src="\assets\photos\IMG-20240822-WA0017.jpg" alt="" /> */}
     </div>
   );
 }
