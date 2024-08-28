9e9b7088ab066b665ab49fbcdda09c7d6a9748fd
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-08-28 19:31:09 +0400

    feat:adding fotter!

---
+++ src/components/Fotter.tsx
@@ -0,0 +1,24 @@
+
+function Fotter() {
+  return (
+    <footer className="bg-black w-full h-full text-[#fff] flex justify-center items-center flex-col">
+      <span className="font-playfair text-[30px]">Euro Mania</span>
+      <div className="flex flex-col items-center justify-center gap-[15px] tracking-[2px] mt-[30px]">
+        <span >მთავარი გვერდი</span>
+        <span>მაისურები</span>
+        <span>პერანგები</span>
+      </div>
+      <p className='text-[12px] leading-[25px] opacity-[0.5] mt-[50px]'>
+        Euro Mania არის თქვენი ერთადერთი ადგილი უნიკალური, მაღალი ხარისხის
+        მოდისათვის. ჩვენ ვართ პატარა გუნდი, რომელიც შედგება სტილის მოყვარულებისა
+        და ვინტაჟური სამოსის სპეციალისტებისგან, რომლებიც მზად ვართ დაგეხმაროთ
+        იპოვოთ შესაბამისი ზედები, რომლებიც გამოხატავენ თქვენს
+        ინდივიდუალურობას. ეწვიეთ ჩვენს ბუტიკს - ჩვენ ღია ვართ კვირაში 7 დღე.
+      </p>
+      <span></span>
+      <div></div>
+    </footer>
+  );
+}
+
+export default Fotter
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Aboutus from "../components/Aboutus";
 import Category from "../components/Category";
+import Fotter from "../components/Fotter";
 import Header from "../components/Header";
 import Seeproduct from "../components/Seeproduct";
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
       <Seeproduct></Seeproduct>
       <Category></Category>
       <Aboutus></Aboutus>
+      <Fotter></Fotter>
     </div>
   );
 }
