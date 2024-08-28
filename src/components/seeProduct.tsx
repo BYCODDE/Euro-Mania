958f7baeeca6e38dfd697ca50f5aa11fbb8f7b13
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-08-28 19:33:18 +0400

    fix:fixing vercel problem!

--- src/components/seeProduct.tsx
+++ src/components/seeProduct.tsx
@@ -11,9 +11,7 @@ function Seeproduct() {
       <h2 className="text-[23px] font-bold leading-[40px] tracking-[1.286px] ">
         POLO-ს ორიგინალი ზედები{" "}
       </h2>
-      {/* <p className="opacity-[0.75] text-[15px]">
-
-      </p> */}
+      
       <button className="flex justify-center items-center mb-[50px] max-w-[160px] max-h-[48px] bg-[#D87D4A] p-[24px]">
         იხილეთ პროდუქტი
       </button>
