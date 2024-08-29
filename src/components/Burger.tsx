94a0ec60e9ac93ecd566a0b490ae74f88ffffa8e
BYCODDE <anaraliev217@gmail.com>
2024-08-29 14:56:21 +0400

    refactor:added link to the components!

---
+++ src/components/Burger.tsx
@@ -0,0 +1,12 @@
+// import Header from "./Header"
+
+
+// function Burger() {
+//   return (
+//     <Burger>
+//       <Header
+//     </Burger>
+//   )
+// }
+
+// export default Burger
--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,30 +1,32 @@
+import { Link } from "react-router-dom";
 import backgroundImage from "/assets/photos/IMG-20240822-WA0029.jpg";
 import backgroundImage2 from "/assets/photos/category.jpeg";
 import arrow from "/assets/svg/arrow.svg";
 function Category() {
   return (
     <div className=" w-full h-full mt-[50px] flex flex-col gap-[60px] items-center p-[24px]">
-      <div
-        className="w-[327px] h-[165px] rounded-lg bg-[#F1F1F1] gap-[20px] flex justify-center items-center flex-col bg-cover bg-center  text-[#000] text-center tracking-[1.071px] font-bold text-[30px] cursor-pointer"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      >
-        მაისურები
-        <span className="text-[23px]  text-white flex justify-center items-center gap-[3px]">
-          შეძენა <img className="w-[15px] h-[15px]" src={arrow} alt="arrow" />
-        </span>
-      </div>
-      <div
-        className="w-[327px] h-[165px] rounded-lg bg-[#F1F1F1] gap-[20px] flex justify-center items-center flex-col bg-cover bg-center  text-[#FFF] text-center tracking-[1.071px] font-bold text-[30px] cursor-pointer"
-        style={{ backgroundImage: `url(${backgroundImage2})` }}
-      >
-        პერანგები
-        <span className="text-[23px]  text-white flex justify-center items-center gap-[3px]">
-          შეძენა <img className="w-[15px] h-[15px]" src={arrow} alt="arrow" />
-        </span>
-      </div>
-      {/* <div className="max-w-[327px] h-[165px] rounded-lg bg-[#F1F1F1]">
-        123123123
-      </div> */}
+      <Link to="/shirts">
+        <div
+          className="w-[327px] h-[165px] rounded-lg bg-[#F1F1F1] gap-[20px] flex justify-center items-center flex-col bg-cover bg-center  text-[#000] text-center tracking-[1.071px] font-bold text-[30px] cursor-pointer"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        >
+          მაისურები
+          <span className="text-[23px]  text-white flex justify-center items-center gap-[3px]">
+            შეძენა <img className="w-[15px] h-[15px]" src={arrow} alt="arrow" />
+          </span>
+        </div>
+      </Link>
+      <Link to="/t-shirts">
+        <div
+          className="w-[327px] h-[165px] rounded-lg bg-[#F1F1F1] gap-[20px] flex justify-center items-center flex-col bg-cover bg-center  text-[#FFF] text-center tracking-[1.071px] font-bold text-[30px] cursor-pointer"
+          style={{ backgroundImage: `url(${backgroundImage2})` }}
+        >
+          პერანგები
+          <span className="text-[23px]  text-white flex justify-center items-center gap-[3px]">
+            შეძენა <img className="w-[15px] h-[15px]" src={arrow} alt="arrow" />
+          </span>
+        </div>
+      </Link>
     </div>
   );
 }
