251c35bfc1170a44b0c41128901341293584fbd4
BYCODDE <anaraliev217@gmail.com>
2024-08-29 14:42:47 +0400

    feat:social media navigation logic added but not done yet!

--- src/components/Fotter.tsx
+++ src/components/Fotter.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import facebook from "/assets/svg/icon-facebook.svg";
 import instagram from "/assets/svg/icon-instagram.svg";
 import messenger from "/assets/svg/messenger.svg";
@@ -5,12 +6,28 @@ function Fotter() {
   return (
     <footer className="bg-black w-full h-full text-[#fff] flex justify-center items-center flex-col text-center">
       <span className="font-playfair text-[30px] mt-[20px]">Euro Mania</span>
-      <div className="flex flex-col items-center justify-center gap-[15px] tracking-[2px] mt-[30px]">
-        <span className="cursor-pointer">მთავარი გვერდი</span>
-        <span className="cursor-pointer">მაისურები</span>
-        <span className="cursor-pointer">პერანგები</span>
+      <div className="  tracking-[2px] mt-[30px]">
+        <nav>
+          <ul className="flex flex-col items-center justify-center gap-[15px]">
+            <li>
+              <Link className="cursor-pointer" to="/">
+                მთავარი გვერდი
+              </Link>
+            </li>
+            <li>
+              <Link className="cursor-pointer" to="/shirts">
+                მაისურები
+              </Link>
+            </li>
+            <li>
+              <Link className="cursor-pointer" to="/t-shirts">
+                პერანგები
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <p className="text-[12px] leading-[25px] opacity-[0.5] mt-[20px] p-[24px]">
+      <p className="text-[14px] leading-[25px] opacity-[0.5] mt-[20px] p-[24px]">
         Euro Mania - ჩვენ ვართ პატარა გუნდი, რომელიც შედგება სტილის
         მოყვარულებისა და ვინტაჟური სამოსის სპეციალისტებისგან, რომლებიც მზად ვართ
         დაგეხმაროთ იპოვოთ შესაბამისი ზედები, რომლებიც გამოხატავენ თქვენს
@@ -20,13 +37,31 @@ function Fotter() {
         Copyright 2024. All Rights Reserved
       </span>
       <div className="flex gap-[20px] mt-[20px] mb-[30px]">
-        <img src={facebook} alt="facebook" className="cursor-pointer" />
-        <img src={instagram} alt="instagram" className="cursor-pointer" />
-        <img
-          src={messenger}
-          alt="messenger"
-          className="w-[24px] h-[24px] cursor-pointer"
-        />
+        <a
+          href="https://www.linkedin.com/in/anaraliev217/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={facebook} alt="facebook" className="cursor-pointer" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/anaraliev217/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={instagram} alt="instagram" className="cursor-pointer" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/anaraliev217/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={messenger}
+            alt="messenger"
+            className="w-[24px] h-[24px] cursor-pointer"
+          />
+        </a>
       </div>
     </footer>
   );
