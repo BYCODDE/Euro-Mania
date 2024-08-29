8b5380338f13cddbd78be71c0d388ddc1ae36d3e
BYCODDE <anaraliev217@gmail.com>
2024-08-29 14:59:09 +0400

    style:refactor of styles and Links!

--- src/components/Headerproduct.tsx
+++ src/components/Headerproduct.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import backgroundImage from "/assets/photos/123123.png";
 function Headerproduct() {
   return (
@@ -9,12 +10,13 @@ function Headerproduct() {
         ახალი პროდუქცია 🔥
       </h3>
       <h2 className="text-[23px] font-bold leading-[40px] tracking-[1.286px] ">
-        POLO-ს ორიგინალი ზედები{" "}
+        POLO-ს ორიგინალი მაისურები{" "}
       </h2>
-
+ <Link to={"/shirts"}>
       <button className="flex justify-center items-center mb-[50px] max-w-[160px] max-h-[48px] bg-[#D87D4A] p-[24px] font-bold rounded-sm">
         იხილეთ პროდუქტი
       </button>
+ </Link>
     </div>
   );
 }
