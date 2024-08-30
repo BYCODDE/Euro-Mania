375688b2d3fd15c6bc04b6399df1ef4449418796
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-08-30 13:19:46 +0400

    fix:fixing vercel problem!

--- src/pages/Shirts.tsx
+++ src/pages/Shirts.tsx
@@ -2,8 +2,7 @@ import Header from "../components/Header";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "swiper/css";
-import "swiper/css/autoplay";
+
 
 function Shirts() {
   const settings = {
